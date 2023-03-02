e2d0d60cad366d5be3230f82b4a0b59b0060897e
Wes Harrison <wesharrison89@gmail.com>
2023-03-01 22:59:09 -0800

    added renderLicenseBadge function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  } else if (license === 'ISC') {
+    return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`;
+  } else if (license === 'Apache 2.0') {
+    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+  } else if (license === 'Mozilla') {
+    return `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
+  } else {
+    return ``;
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
